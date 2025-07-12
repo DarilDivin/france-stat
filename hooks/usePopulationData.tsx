779f42d20467458b8f6f16a5eaa9f78582d67443
Daril DJODJO KOUTON <109483193+DarilDivin@users.noreply.github.com>
2025-07-12 02:19:49 +0200

    fix: passage d'appel api à un appel json

--- hooks/usePopulationData.tsx
+++ hooks/usePopulationData.tsx
@@ -29,8 +29,8 @@ export function PopulationContextProvider({
   const [selectedDep, setSelectedDep] = useState<PopulationDepartement | null>(null);
 
   useEffect(() => {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "") || "";
-    const url = apiUrl ? `${apiUrl}/api/population` : "/api/population";
+    // const apiUrl = process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "") || "";
+    // const url = apiUrl ? `${apiUrl}/api/population` : "/api/population";
     // fetch("http://localhost:8000/api/population")
     // fetch(url)
     fetch("/data/population.json")
